c9638ddda8601919be6ad46449fc5f7745539747
norberta <norbertahp@163.com>
2019-04-30 12:49:28 +0800

    '我的申报'

--- resource/src/common/header/index.js
+++ resource/src/common/header/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import logoimg from '../../statics/logo.png'
+import avatar from '../../statics/avatar.png'
 import { connect } from 'react-redux';
-import headers from './style.scss'
+import './style.scss'
 
 function mapStateToProps(state) {
   return {};
@@ -8,18 +10,38 @@ function mapStateToProps(state) {
 
 class Header extends Component {
   render() {
-
-    console.log(headers)
     return (
-      <div className={`${headers.m}`}>
-       miu
+      <div className="header-background">
+
+        <div className='logo-block'>
+          <img className="logo" src={ logoimg } alt=''/>
+          <p className="logo-name">奖项申报系统</p>
+        </div>
+
+        <div className='content'>
+          <div className='manage content-box'>
+            系统管理
+          </div>
+          <div className='check content-box'>
+            我的审核
+          </div>
+          <div className='apply content-box'>
+            我的申报
+          </div>
+        </div>
+
+        <div className='userinfo'>
+          <img className='avatar' src={ avatar } alt=''/>
+          <div className='nickname'>
+            norberta
+          </div>
+        </div>
+
       </div>
     );
   }
 }
 
-// export default connect(
-//   mapStateToProps,
-// )(Header);
-
-export default Header;
+export default connect(
+  mapStateToProps,
+)(Header);
